b35854592ab83d466b1e1b7c3cacf6d7b08dfa16
hoinx-0899 <nguyen.xuan.hoi@sun-asterisk.com>
2019-08-16 09:46:16 +0700

    model and base recycler

--- app/src/main/java/com/sun/moviedb/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/sun/moviedb/data/remote/RemoteDataSource.kt
@@ -1,3 +1,17 @@
 package com.sun.moviedb.data.remote
 
-interface RemoteDataSource
+import androidx.lifecycle.LiveData
+import com.sun.moviedb.base.BaseResponse
+import com.sun.moviedb.data.dto.GenresDto
+import com.sun.moviedb.data.dto.MovieDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface RemoteDataSource {
+    @GET("genre/movie/list")
+    fun getGenres(): LiveData<BaseResponse<GenresDto>>
+
+    @GET("movie/{type}")
+    fun getMoviesByCategory(@Path("type") type: String, @Query("page") page: Int): LiveData<BaseResponse<MovieDto>>
+}
